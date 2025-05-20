c66d6aa43399e9d530a42c237edede050a4fd7a6
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-20 14:28:18 +0530

    externalize configuration, replacing application.properties with application.yaml file

--- store/src/main/java/com/sri/store/OrderService.java
+++ store/src/main/java/com/sri/store/OrderService.java
@@ -10,7 +10,7 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
+    public OrderService(@Qualifier("stripe") PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
--- store/src/main/java/com/sri/store/StripePaymentService.java
+++ store/src/main/java/com/sri/store/StripePaymentService.java
@@ -1,13 +1,31 @@
 package com.sri.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service("stripe")
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiurl}")
+    private String apiUrl;
+
+    @Value("${stripe.enabled}")
+    private boolean enebled;
+
+    @Value("${stripe.timeout:3000}")
+    private int timeout;
+
+    @Value("${stripe.supported-currencies}")
+    private List<String> supportedCurrencies;
     @Override
     public void processPayment(double amount) {
         System.out.println("STRIPE");
+        System.out.println("API URL: " + apiUrl);
+        System.out.println("ENEBLED: " + enebled);
+        System.out.println("TIMEOUT: " + timeout);
+        System.out.println("SUPPORTED CURRENCIES: " + supportedCurrencies);
         System.out.println("Amount: " + amount);
     }
 }
