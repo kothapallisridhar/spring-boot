eece26131e3ffa9245bf31830a33449715b6a2dc
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-20 16:47:38 +0530

    clean up,

--- store/src/main/java/com/sri/store/NotificationService.java
+++
@@ -1,5 +0,0 @@
-package com.sri.store;
-
-public interface NotificationService {
-    void send(String message);
-}
--- store/src/main/java/com/sri/store/StoreApplication.java
+++ store/src/main/java/com/sri/store/StoreApplication.java
@@ -3,12 +3,14 @@ package com.sri.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		//ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 		//orderService.setPaymentService(new PaypalPaymentService());
 		var orderService = context.getBean(OrderService.class);
 		orderService.placeOrder();
@@ -17,7 +19,7 @@ public class StoreApplication {
 		notificationManager.sendNotification("using Spring Boot");
 
 		var resource = context.getBean(HeavyResource.class);
-
+		context.close();
 	}
 
 }
