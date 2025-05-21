39eeea21eead35411431168b8c139f2890a40a7e
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-21 18:15:22 +0530

    add entity class

--- store/src/main/java/com/sri/store/StoreApplication.java
+++ store/src/main/java/com/sri/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.sri.store;
 
+import com.sri.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,15 +12,7 @@ public class StoreApplication {
 	public static void main(String[] args) {
 		//ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 		ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		//orderService.setPaymentService(new PaypalPaymentService());
-		var orderService = context.getBean(OrderService.class);
-		orderService.placeOrder();
 
-		NotificationManager notificationManager = context.getBean(NotificationManager.class);
-		notificationManager.sendNotification("using Spring Boot");
-
-		var resource = context.getBean(HeavyResource.class);
-		context.close();
 	}
 
 }
