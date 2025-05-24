37357a454ddc7908052b6ffb7aefcbe3a995d7e6
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-24 13:30:03 +0530

    JPA tables auto generation

--- sbjpa-tables-auto-generate/src/main/java/com/sri/sbjpa/SbjpaTablesAutoGenerateApplication.java
+++ sbjpa-tables-auto-generate/src/main/java/com/sri/sbjpa/SbjpaTablesAutoGenerateApplication.java
@@ -1,13 +1,18 @@
 package com.sri.sbjpa;
 
+import com.sri.sbjpa.operations.OrderOperations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SbjpaTablesAutoGenerateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SbjpaTablesAutoGenerateApplication.class, args);
+
+        ConfigurableApplicationContext container = SpringApplication.run(SbjpaTablesAutoGenerateApplication.class, args);
+        OrderOperations orderOperations = container.getBean(OrderOperations.class);
+        orderOperations.loadAllOrders();
     }
 
 }
