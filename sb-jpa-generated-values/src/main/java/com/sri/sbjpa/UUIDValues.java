6adb1e8abf44e354456762d7bc4568edcc7af266
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-28 22:33:19 +0530

    generate UUID value

---
+++ sb-jpa-generated-values/src/main/java/com/sri/sbjpa/UUIDValues.java
@@ -0,0 +1,10 @@
+package com.sri.sbjpa;
+
+import java.util.UUID;
+
+public class UUIDValues {
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println("UUID value: " + uuid);
+    }
+}
