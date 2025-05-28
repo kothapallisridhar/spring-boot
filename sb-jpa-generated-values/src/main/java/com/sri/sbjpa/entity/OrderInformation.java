15083fb9719cc463b72fe35589a7c47fa70f6047
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-28 22:31:36 +0530

    UUID as primary key

--- sb-jpa-generated-values/src/main/java/com/sri/sbjpa/entity/OrderInformation.java
+++ sb-jpa-generated-values/src/main/java/com/sri/sbjpa/entity/OrderInformation.java
@@ -9,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Data
@@ -17,7 +18,7 @@ import java.time.LocalDateTime;
 public class OrderInformation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long orderId;
+    private UUID orderId;
     private String emailId;
     private String name;
     private double price;
