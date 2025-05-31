42bf8650b887e77de1fd42644465df0844d2579f
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-31 15:00:18 +0530

    fix view resolver path issue

--- spring-boot-web/src/main/java/com/sri/web/controller/HomeController.java
+++ spring-boot-web/src/main/java/com/sri/web/controller/HomeController.java
@@ -1,7 +1,6 @@
-package com.sri.web;
+package com.sri.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +9,7 @@ public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
     public String sayWelcome() {
+        System.out.println("Request received by Controller. Processing request...");
         return "welcome";
     }
 }
--- spring-boot-web/src/main/webapp/WEB-INF/view/welcome.jsp
+++ spring-boot-web/src/main/webapp/WEB-INF/view/welcome.jsp
@@ -1,2 +1,2 @@
 
-<h1>Welcome to Spring Boot Web moudle training</h1>
+<h1>Welcome to Spring Boot Web module training</h1>
