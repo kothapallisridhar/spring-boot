86523eda80b3bf32a4b0407ff66ac6e0c0f9431f
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-05-31 14:36:47 +0530

    add Controller

--- spring-boot-web/src/main/java/com/sri/web/HomeController.java
+++ spring-boot-web/src/main/java/com/sri/web/HomeController.java
@@ -2,11 +2,13 @@ package com.sri.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/hello")
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
     public String sayWelcome() {
         return "welcome";
     }
