0429ea5a82913ece33538e09094e15d1f28fb0a7
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-01 09:42:44 +0530

    add custom findByFullName method in repository

--- user-management/src/main/java/com/sri/repository/UserManagementRepository.java
+++ user-management/src/main/java/com/sri/repository/UserManagementRepository.java
@@ -4,5 +4,5 @@ import com.sri.entity.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserManagementRepository extends JpaRepository<UserDetails, String> {
-
+    public UserDetails findByFullName(String fullName);
 }
