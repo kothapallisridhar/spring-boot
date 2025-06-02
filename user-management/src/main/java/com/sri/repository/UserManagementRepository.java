a58a90b748dc85f16c4fc17ff0651a5b877ce494
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-02 23:14:24 +0530

    refactor

--- user-management/src/main/java/com/sri/repository/UserManagementRepository.java
+++ user-management/src/main/java/com/sri/repository/UserManagementRepository.java
@@ -4,6 +4,8 @@ import com.sri.entity.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserManagementRepository extends JpaRepository<UserDetails, String> {
+
     public UserDetails findByFullName(String fullName);
     public UserDetails findByEmailIdAndPassword(String email, String password);
+
 }
