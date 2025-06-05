3de0965cc659f0b7b2cb45522ff2305b3228538e
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-05 11:11:11 +0530

    add RessopnseBody annotation, welcome to REST services

---
+++ srk-services/src/main/java/com/sri/srkservices/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.sri.srkservices.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    // Req: Welcome to REST Services by SRK
+    @ResponseBody
+    @RequestMapping(path = "/welcome", method = RequestMethod.GET)
+    public String sendWelcomeMessage() {
+        return "Welcome to REST services by SRK services";
+    }
+}
