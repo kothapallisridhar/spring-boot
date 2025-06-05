5cd0cb3ceadb5d088304ca17df5507b60fab1f68
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-05 10:57:25 +0530

    initial commit

---
+++ srk-services/src/main/java/com/sri/srkservices/SrkServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sri.srkservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrkServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrkServicesApplication.class, args);
+    }
+
+}
---
+++ srk-services/src/test/java/com/sri/srkservices/SrkServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.srkservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrkServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
