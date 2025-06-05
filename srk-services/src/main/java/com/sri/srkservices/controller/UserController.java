9d8beddfe37de4e385d97d2a14af8dd5e753419f
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-05 11:27:32 +0530

    using @RestCtroller

--- srk-services/src/main/java/com/sri/srkservices/controller/UserController.java
+++ srk-services/src/main/java/com/sri/srkservices/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     // Not added @ResponseBody but internally added because of @RestController
+
+    // REST Service
+
     @RequestMapping(path = "/add/user", method = RequestMethod.GET)
     public String addUser() {
 
