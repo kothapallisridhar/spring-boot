ba76727ff918d1080b439dad9ba0aaa277e95c1f
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-06 23:40:18 +0530

    add spring-boot-devtools

--- srk-services/src/main/java/com/sri/srkservices/controller/UserController.java
+++ srk-services/src/main/java/com/sri/srkservices/controller/UserController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     // Not added @ResponseBody but internally added because of @RestController
-
     // REST Service
-
     @RequestMapping(path = "/add/user", method = RequestMethod.GET)
     public String addUser() {
         return "User added successfully";
