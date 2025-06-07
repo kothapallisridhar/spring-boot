19577b30f8d6232fd7ba662eeff3ebff3dbe032d
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-07 19:06:56 +0530

    using value (in place of path)

--- rest-services-http-methods-json/src/main/java/com/sri/restservices/controller/OrganizationController.java
+++ rest-services-http-methods-json/src/main/java/com/sri/restservices/controller/OrganizationController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrganizationController {
     // REST Service
     // an end point shold allow all HTTP methods if they don't define HTTP method in request mapping
-    @RequestMapping(path = "/info")
+    // value = path
+    @RequestMapping(value = "/info")
     public String getInfo() {
         return "SRK IT";
     }
