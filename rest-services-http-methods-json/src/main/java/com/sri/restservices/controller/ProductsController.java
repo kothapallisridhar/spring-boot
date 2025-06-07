3bed93c3d642231ecf6335b7cb9ca917916415b7
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-07 18:50:35 +0530

    refactor

---
+++ rest-services-http-methods-json/src/main/java/com/sri/restservices/controller/ProductsController.java
@@ -0,0 +1,8 @@
+package com.sri.restservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class ProductsController {
+
+}
