71788337615aeae18a3fea16d2ccf6a4fb048e51
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-11 12:04:53 +0530

    default response type

--- rest-services-json-request-body/src/main/java/com/sri/restservices/controller/UserAccountsController.java
+++ rest-services-json-request-body/src/main/java/com/sri/restservices/controller/UserAccountsController.java
@@ -1,16 +1,12 @@
 package com.sri.restservices.controller;
 
+import com.sri.restservices.request.Address;
 import com.sri.restservices.request.AddressRequest;
 import com.sri.restservices.request.UserSignupRequest;
 import com.sri.restservices.service.UserAccountsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -20,7 +16,7 @@ public class UserAccountsController {
     private UserAccountsService userAccountsService;
 
     // HTTP method: Create: POST
-    @PostMapping(path = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
     public String userSignup(@RequestBody UserSignupRequest userSignupRequest) {
         System.out.println("User signup request: " + userSignupRequest);
         String result = userAccountsService.userSignUp(userSignupRequest);
@@ -34,5 +30,22 @@ public class UserAccountsController {
         // service
         System.out.println("User address request: " + addressRequest);
         return "Address added successfully";
+    }
+
+    @GetMapping("/get/address")
+    public Address getAddress() {
+
+        Address address = new Address();
+        address.setArea("Bangalore");
+        address.setCity("Bangalore");
+        address.setCountry("India");
+        address.setDistrict("Bangalore");
+        address.setFlatDetails("1122/abc");
+        address.setLandmark("Near Airport");
+        address.setPincode("566066");
+        address.setState("Karnataka");
+
+        // returning response content
+        return address;
     }
 }
---
+++ rest-services-json-request-body/src/main/java/com/sri/restservices/request/Address.java
@@ -0,0 +1,15 @@
+package com.sri.restservices.request;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    private String area;
+    private String flatDetails;
+    private String city;
+    private String pincode;
+    private String landmark;
+    private String district;
+    private String state;
+    private String country;
+}
