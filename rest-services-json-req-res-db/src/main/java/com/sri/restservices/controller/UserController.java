c45b59de66a581795cd8ea23859448f016b198a1
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-12 23:14:17 +0530

    update consumes MediaTypes, both JSON and XML

--- rest-services-json-req-res-db/src/main/java/com/sri/restservices/controller/UserController.java
+++ rest-services-json-req-res-db/src/main/java/com/sri/restservices/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sri.restservices.controller;
 import com.sri.restservices.request.UserDetailsRequest;
 import com.sri.restservices.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class UserController {
     @Autowired
     public UserService userService;
 
-    @PostMapping("/user/create")
+    @PostMapping(path = "/user/create", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public String createUser(@RequestBody UserDetailsRequest userDeatilsRequest) {
 
         // pass this info to service layer
