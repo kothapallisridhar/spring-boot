d0f98ba754e7a9328349b2df18b346f9991eb3c8
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-14 21:03:11 +0530

    user Login service

--- rest-services-json-req-res-db/src/main/java/com/sri/restservices/controller/UserController.java
+++ rest-services-json-req-res-db/src/main/java/com/sri/restservices/controller/UserController.java
@@ -17,8 +17,8 @@ public class UserController {
     @Autowired
     public UserService userService;
 
-    @PostMapping(path = "/user/create", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PostMapping(path = "/user/create", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public String createUser(@RequestBody UserDetailsRequest userDeatilsRequest) {
 
         // pass this info to service layer
@@ -32,5 +32,4 @@ public class UserController {
         UserDetailsResponse response = userService.userLogin(userLoginRequest);
         return response;
     }
-
 }
