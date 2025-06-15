a818f912a66056ff873899074f3d7c462d77e639
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-15 21:01:32 +0530

    user Login service

--- rest-services-json-req-res-db/src/main/java/com/sri/restservices/repository/UserRepository.java
+++ rest-services-json-req-res-db/src/main/java/com/sri/restservices/repository/UserRepository.java
@@ -4,6 +4,5 @@ import com.sri.restservices.entity.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserDetails, String> {
-
     UserDetails findByEmailIdAndPassword(String emailId, String password);
 }
