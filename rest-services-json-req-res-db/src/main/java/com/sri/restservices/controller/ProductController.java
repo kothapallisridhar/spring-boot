4de576760e3abb7a79b9e1947813086b2f0bc541
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-16 22:50:12 +0530

    add ProductController

--- rest-services-json-req-res-db/src/main/java/com/sri/restservices/controller/ProductController.java
+++ rest-services-json-req-res-db/src/main/java/com/sri/restservices/controller/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-
     @PostMapping("/add/product")
     public String addProduct() {
         return "Product added";
