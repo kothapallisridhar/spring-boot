9c2027665ffb4e3042ca9117713633cf2f6bc5dc
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-19 10:33:07 +0530

    user-service

--- microservices/order-service/src/main/java/com/sri/orderservice/entiry/ProductDetails.java
+++ microservices/order-service/src/main/java/com/sri/orderservice/entiry/ProductDetails.java
@@ -1,6 +1,7 @@
 package com.sri.orderservice.entiry;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "amazon_order_info")
 public class ProductDetails {
 
+    @Id
     private Integer productId;
     private String productName;
     private double price;
