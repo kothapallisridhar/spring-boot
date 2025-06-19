9f32133ada0d3d53419172cf5c0f603b0d170b45
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-06-19 11:04:09 +0530

    api gateway

---
+++ microservices/amazon-api-gateway/src/test/java/com/sri/amazonapigateway/AmazonApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.amazonapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
