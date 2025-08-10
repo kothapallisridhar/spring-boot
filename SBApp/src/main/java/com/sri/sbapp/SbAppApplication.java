b32e71d1b741404c60645fa710f22b1a99c27c12
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-10 12:32:48 +0530

    bean definition count

--- SBApp/src/main/java/com/sri/sbapp/SbAppApplication.java
+++ SBApp/src/main/java/com/sri/sbapp/SbAppApplication.java
@@ -2,12 +2,14 @@ package com.sri.sbapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SbAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SbAppApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SbAppApplication.class, args);
+        System.out.println("Bean Definition Count: " + context.getBeanDefinitionCount());
     }
 
 }
