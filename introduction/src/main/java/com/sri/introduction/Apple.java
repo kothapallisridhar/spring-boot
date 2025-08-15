e39bf01a5b13f60d76c4dadd4fa42a5dca8b6cd8
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-15 18:32:22 +0530

    Spring intro app

---
+++ introduction/src/main/java/com/sri/introduction/Apple.java
@@ -0,0 +1,10 @@
+package com.sri.introduction;
+
+import org.springframework.stereotype.Component;
+
+//@Component
+public class Apple {
+    public void eatApple() {
+        System.out.println("Im eating apple");
+    }
+}
