1a82e76c118b4fa59c1997214a83f409fe5aa413
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2025-08-18 16:07:49 +0530

    life cycle hooks

--- introduction/src/main/java/com/sri/introduction/Apple.java
+++ introduction/src/main/java/com/sri/introduction/Apple.java
@@ -1,10 +1,22 @@
 package com.sri.introduction;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 //@Component
 public class Apple {
     public void eatApple() {
         System.out.println("Im eating apple");
+    }
+
+    @PostConstruct
+    void callThisBeforeAppleCreated() {
+        System.out.println("Apple object is going to be created");
+    }
+
+    @PreDestroy
+    void callThisBeforeAppleDestroyed() {
+        System.out.println("Apple object is going to be destroyed");
     }
 }
