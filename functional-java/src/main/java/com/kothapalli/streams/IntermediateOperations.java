83bb7f100335c039c5cea5a7ae99141e7d489145
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2026-02-22 23:00:20 +0530

    Streams demo intermediate operations

--- functional-java/src/main/java/com/kothapalli/streams/IntermediateOperations.java
+++ functional-java/src/main/java/com/kothapalli/streams/IntermediateOperations.java
@@ -14,6 +14,14 @@ public class IntermediateOperations {
         numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         numbers.filter(i -> i % 2 == 0).limit(2).forEach(System.out::println);
 
+        Stream<String> fruitsStream = Stream.of("apple", "banana", "mango", "grape", "orange", "strawberry");
+        fruitsStream
+                .map(String::toUpperCase)
+                .peek(str -> System.out.println("Peek result: " + str))  // to do any kind of a side effect like printing / peek the result/ peek is used to examine something
+                .filter(name -> name.length() < 6)
+                .forEach(System.out::println);
+
+
 
 
 
